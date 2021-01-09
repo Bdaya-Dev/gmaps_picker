ebcc421c4ec0447eb3d6e988c084b0a835e2cd89
Sharad Chand <pepsighan@sharadchand.com>
2021-01-09 12:48:19 +0545

    Cleanup the example to focus just of map

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,38 +7,12 @@ const apiKey = 'put-your-api-key-here';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // Light Theme
-  final ThemeData lightTheme = ThemeData.light().copyWith(
-    // Background color of the FloatingCard
-    cardColor: Colors.white,
-    buttonTheme: ButtonThemeData(
-      // Select here's button color
-      buttonColor: Colors.black,
-      textTheme: ButtonTextTheme.primary,
-    ),
-  );
-
-  // Dark Theme
-  final ThemeData darkTheme = ThemeData.dark().copyWith(
-    // Background color of the FloatingCard
-    cardColor: Colors.grey,
-    buttonTheme: ButtonThemeData(
-      // Select here's button color
-      buttonColor: Colors.yellow,
-      textTheme: ButtonTextTheme.primary,
-    ),
-  );
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Google Map Place Picker Demo',
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      themeMode: ThemeMode.light,
       home: HomePage(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -53,89 +27,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  PickResult selectedPlace;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Google Map Place Picer Demo'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              RaisedButton(
-                child: Text('Load Google Map'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return PlacePicker(
-                          apiKey: apiKey,
-                          initialPosition: HomePage.kInitialPosition,
-                          useCurrentLocation: true,
-                          selectInitialPosition: true,
-
-                          //usePlaceDetailSearch: true,
-                          onPlacePicked: (result) {
-                            selectedPlace = result;
-                            Navigator.of(context).pop();
-                            setState(() {});
-                          },
-                          //forceSearchOnZoomChanged: true,
-                          //automaticallyImplyAppBarLeading: false,
-                          //autocompleteLanguage: "ko",
-                          //region: 'au',
-                          //selectInitialPosition: true,
-                          selectedPlaceWidgetBuilder:
-                              (_, selectedPlace, state, isSearchBarFocused) {
-                            print(
-                                'state: $state, isSearchBarFocused: $isSearchBarFocused');
-                            return isSearchBarFocused
-                                ? Container()
-                                : FloatingCard(
-                                    bottomPosition:
-                                        0.0, // MediaQuery.of(context) will cause rebuild. See MediaQuery document for the information.
-                                    leftPosition: 0.0,
-                                    rightPosition: 0.0,
-                                    width: 500,
-                                    borderRadius: BorderRadius.circular(12.0),
-                                    child: state == SearchingState.Searching
-                                        ? Center(
-                                            child: CircularProgressIndicator())
-                                        : RaisedButton(
-                                            child: Text('Pick Here'),
-                                            onPressed: () {
-                                              // IMPORTANT: You MUST manage selectedPlace data yourself as using this build will not invoke onPlacePicker as
-                                              //            this will override default 'Select here' Button.
-                                              print(
-                                                  'do something with [selectedPlace] data');
-                                              Navigator.of(context).pop();
-                                            },
-                                          ),
-                                  );
-                          },
-                          // pinBuilder: (context, state) {
-                          //   if (state == PinState.Idle) {
-                          //     return Icon(Icons.favorite_border);
-                          //   } else {
-                          //     return Icon(Icons.favorite);
-                          //   }
-                          // },
-                        );
-                      },
-                    ),
-                  );
-                },
+      appBar: AppBar(
+        title: Text('Google Map Place Picer Demo'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Load Google Map'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => _buildPlacePicker(),
               ),
-              selectedPlace == null
-                  ? Container()
-                  : Text(selectedPlace.formattedAddress ?? ''),
-            ],
-          ),
-        ));
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPlacePicker() {
+    return PlacePicker(
+      apiKey: apiKey,
+      initialPosition: HomePage.kInitialPosition,
+      useCurrentLocation: true,
+      selectInitialPosition: true,
+      onPlacePicked: (result) {
+        print('You picked: ${result.formattedAddress}');
+        Navigator.of(context).pop();
+      },
+    );
   }
 }
