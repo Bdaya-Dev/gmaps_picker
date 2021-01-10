e26bc8f6f73f3254962ae29e55bc84c3fe528e76
Sharad Chand <pepsighan@sharadchand.com>
2021-01-10 20:15:10 +0545

    No need of margin

--- lib/src/autocomplete_search.dart
+++ lib/src/autocomplete_search.dart
@@ -10,30 +10,26 @@ class AutoCompleteSearchState extends State<AutoCompleteSearch> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(left: 16, right: 8),
-      alignment: Alignment.center,
-      child: Material(
-        elevation: 3,
-        borderRadius: BorderRadius.circular(4),
-        child: TextField(
-          controller: controller,
-          decoration: InputDecoration(
-            hintText: 'Search for a locality, landmark or city',
-            isDense: true,
-            contentPadding: EdgeInsets.symmetric(vertical: 12),
-            border: OutlineInputBorder(
-              borderSide: BorderSide.none,
-              borderRadius: BorderRadius.circular(4),
-            ),
-            prefixIcon: Container(
-              padding: EdgeInsets.symmetric(horizontal: 12),
-              child: Icon(Icons.search, size: 24),
-            ),
-            prefixIconConstraints: BoxConstraints.loose(Size.fromHeight(32)),
-            fillColor: Colors.white,
-            filled: true,
+    return Material(
+      elevation: 3,
+      borderRadius: BorderRadius.circular(4),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          hintText: 'Search for a locality, landmark or city',
+          isDense: true,
+          contentPadding: EdgeInsets.symmetric(vertical: 12),
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(4),
           ),
+          prefixIcon: Container(
+            padding: EdgeInsets.symmetric(horizontal: 12),
+            child: Icon(Icons.search, size: 24),
+          ),
+          prefixIconConstraints: BoxConstraints.loose(Size.fromHeight(32)),
+          fillColor: Colors.white,
+          filled: true,
         ),
       ),
     );
